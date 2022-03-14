2f57b609831ea9072bbc8f4c87fb329678d6f36a
Gilbert138 <gilbertpalentein8@gmail.com>
2022-03-14 23:51:37 +0700

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navigation/UserNavbar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Navbar,
+  },
+};
 </script>
 
 <style>
@@ -23,6 +28,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Navigation/UserNavbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="header">
+    <b-navbar shadow>
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img src="@/assets/Images/YukBayar.png" />
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item href="/">
+          <span class="nav__item">Home</span>
+        </b-navbar-item>
+        <b-navbar-item href="/menu">
+          <span class="nav__item">Top Up</span>
+        </b-navbar-item>
+        <b-navbar-item href="/order">
+          <span class="nav__item">History</span>
+        </b-navbar-item>
+        <b-navbar-item href="/about">
+          <span class="nav__item">Register</span>
+        </b-navbar-item>
+        <b-navbar-item href="/tracking">
+          <span class="nav__item">Login</span>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #152349;
+  z-index: 100;
+}
+</style>
