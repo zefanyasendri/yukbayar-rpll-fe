2cfba3b2e00c009cb3a41cdf599496ee18ec3d1f
Gilbert138 <gilbertpalentein8@gmail.com>
2022-04-20 00:21:07 +0700

    Integrasi Profile View

--- src/views/Customer/ProfileView.vue
+++ src/views/Customer/ProfileView.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="container is-max-desktop">
-
     <div class="columns">
       <div class="column is-5"></div>
       <div class="column is-1 mt-3">
         <figure class="image is-128x128">
-          <img src="https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg">
+          <img
+            src="https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg"
+          />
         </figure>
       </div>
     </div>
@@ -14,9 +15,9 @@
       <div class="column is-3 has-text-left mt-3">
         <label class="label subtitle is-4">Name</label>
       </div>
-      <div class="column is-1 mt-3"> : </div>
+      <div class="column is-1 mt-3">:</div>
       <div class="control column is-8">
-        <input class="input subtitle is-4" type="text" value="Alex Smith" disabled>
+        {{ dataProfile.nama }}
       </div>
     </div>
 
@@ -24,19 +25,19 @@
       <div class="column is-3 has-text-left mt-3">
         <label class="label subtitle is-4">Email</label>
       </div>
-      <div class="column is-1 mt-3"> : </div>
+      <div class="column is-1 mt-3">:</div>
       <div class="control column is-8">
-        <input class="input subtitle is-4" type="email" value="alexsmith@gmail.com" disabled>
+        {{ dataProfile.email }}
       </div>
     </div>
-    
+
     <div class="field columns is-family-sans-serif">
       <div class="column is-3 has-text-left mt-3">
         <label class="label subtitle is-4">Password</label>
       </div>
-      <div class="column is-1 mt-3"> : </div>
+      <div class="column is-1 mt-3">:</div>
       <div class="control column is-8">
-        <input class="input subtitle is-4" type="password" value="***************" disabled>
+        {{ dataProfile.password }}
       </div>
     </div>
 
@@ -44,9 +45,9 @@
       <div class="column is-3 has-text-left mt-3">
         <label class="label subtitle is-4">No. Telp</label>
       </div>
-      <div class="column is-1 mt-3"> : </div>
+      <div class="column is-1 mt-3">:</div>
       <div class="control column is-8">
-        <input class="input subtitle is-4" type="text" value="081549384953" disabled>
+        {{ dataProfile.noTelpon }}
       </div>
     </div>
 
@@ -54,9 +55,9 @@
       <div class="column is-3 has-text-left mt-3">
         <label class="label subtitle is-4">Tgl. Lahir</label>
       </div>
-      <div class="column is-1 mt-3"> : </div>
+      <div class="column is-1 mt-3">:</div>
       <div class="control column is-8">
-        <input class="input subtitle is-4" type="text" value="17 - 8 - 2001" disabled>
+        {{ dataProfile.tglLahir }}
       </div>
     </div>
 
@@ -64,16 +65,18 @@
       <div class="column is-3 has-text-left mt-3">
         <label class="label subtitle is-4">Gender</label>
       </div>
-      <div class="column is-1 mt-3"> : </div>
+      <div class="column is-1 mt-3">:</div>
       <div class="control column is-8">
-        <input class="input subtitle is-4" type="text" value="Laki-laki" disabled>
+        {{ dataProfile.gender }}
       </div>
     </div>
 
     <div class="field">
       <p class="control">
         <router-link to="/Profile/Update">
-          <button class="button is-rounded has-text-weight-bold subtitle is-5">Ubah Profile</button>
+          <button class="button is-rounded has-text-weight-bold subtitle is-5">
+            Ubah Profile
+          </button>
         </router-link>
       </p>
     </div>
@@ -81,26 +84,42 @@
 </template>
 
 <script>
-
+import axios from "axios";
+export default {
+  mounted() {
+    this.fetchData();
+  },
+  data() {
+    return {
+      dataProfile: [],
+    };
+  },
+  methods: {
+    async fetchData() {
+      const res = await axios.get("/users/1");
+      this.dataProfile = res.data.data;
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .container {
-        padding-top: 1rem;
-  }
-  .input {
-    cursor: auto;
-  }
-  .button {
-    background-color: #223196;
-    color: white;
-    padding: 1rem 5rem;
-  }
-  .button:hover {
-    color: white;
-    background-color: #192676;
-  }
-  .input {
-    background-color: rgb(250, 250, 250);
-  }
+.container {
+  padding-top: 1rem;
+}
+.input {
+  cursor: auto;
+}
+.button {
+  background-color: #223196;
+  color: white;
+  padding: 1rem 5rem;
+}
+.button:hover {
+  color: white;
+  background-color: #192676;
+}
+.input {
+  background-color: rgb(250, 250, 250);
+}
 </style>
