1beeb0495ec1ec15de82c345a3e0661dbf617420
Zefanya Sendri <zefanyasendri@gmail.com>
2022-05-03 21:41:50 +0700

    Add Income Feature

--- src/views/BusinessOwner/PendapatanView.vue
+++ src/views/BusinessOwner/PendapatanView.vue
@@ -1,144 +1,94 @@
 <template>
-<div class="all">     
-    <div class="tile is-ancestor">
-        <div class="tile is-vertical"  style="width:30rem;height:20rem;margin-left:2%;margin-right:2%;">
-            <div class="tile">
-                <div class="tile is-parent is-vertical">
-                    <article class="tile is-child notification">
-                        <div class="box" style="height:8rem;">
-                            <article class="media">
-                                <div class="media-content">
-                                    <div class="content">
-                                        <p>
-                                        <strong>Rp {{transactionServices.formatPrice(income.sebulan)}}</strong>
-                                        <br><br>
-                                        <span style="text-transform:uppercase;"> pendapatan selama 30 hari terakhir</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </article>
+<div class="income">
+    <section class="section">
+        <div class="container">
+            <div class="columns mt-1 is-12 is-variable is-centered is-family-sans-serif">
+                <div class="column is-4-tablet is-4-desktop">
+                    <div class="card">
+                        <div class="card-content">
+                            <p class="title is-4 pt-5">Rp {{transactionServices.formatPrice(income.sebulan)}}</p>
+                            <p class="subtitle is-uppercase pt-5 is-5">pendapatan selama 30 hari terakhir</p>
                         </div>
-                    </article>
-                    <article class="tile is-child notification">
-                        <div class="box" style="height:8rem;">
-                            <article class="media">
-                                <div class="media-content">
-                                    <div class="content">
-                                        <p>
-                                        <strong>Rp {{transactionServices.formatPrice(income.perkiraanbeban)}}</strong>
-                                        <br><br>
-                                        <span style="text-transform:uppercase;"> perkiraan beban sebulan</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </article>
+                    </div>
+                </div>
+
+                <div class="column is-4-tablet-is-4-desktop">
+                    <div class="card">
+                        <div class="card-content">
+                            <p class="title is-4 pt-5">{{income.penggunaaktifsebulan}}</p>
+                            <p class="subtitle is-5 is-uppercase pt-5">pengguna yang aktif selama 30 hari terakhir</p>
                         </div>
-                    </article>
+                    </div>
+                </div>
+
+                <div class="column is-4-tablet-is-4-desktop">
+                    <div class="card">
+                        <div class="card-content">
+                            <p class="title is-4 pt-5">{{income.transaksisebulan}}</p>
+                            <p class="subtitle is-uppercase pt-5 is-5">transaksi selama 30 hari terakhir</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="columns mt-5 is-12 is-variable is-centered is-family-sans-serif">
+                <div class="column is-4-tablet is-4-desktop">
+                    <div class="card">
+                        <div class="card-content">
+                            <p class="title is-4 pb-5 pt-5">
+                                Rp {{transactionServices.formatPrice(income.perkiraanbeban)}} 
+                            </p>
+                            <p class="subtitle is-uppercase pt-5 is-5">perkiraan beban sebulan</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column is-4-tablet-is-4-desktop">
+                    <div class="card">
+                        <div class="card-content">
+                            <p class="title is-4 pb-5 pt-5">Rp {{transactionServices.formatPrice(income.perkiraanpenghasilan-income.perkiraanbeban)}} - Rp {{transactionServices.formatPrice(income.perkiraanpenghasilan+income.perkiraanbeban)}}</p>
+                            <p class="subtitle is-5 is-uppercase pt-5">perkiraan penghasilan sebulan</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column is-4-tablet-is-4-desktop">
+                    <div class="card">
+                        <div class="card-content">
+                            <p class="title is-4 pt-5">Rp {{transactionServices.formatPrice((income.perkiraanpenghasilan-income.perkiraanbeban) * 12)}} - Rp {{transactionServices.formatPrice((income.perkiraanpenghasilan+income.perkiraanbeban) * 12)}}</p>
+                            <p class="subtitle is-uppercase pt-5 is-5">perkiraan penghasilan setahun</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="tile is-ancestor">
-        <div class="tile is-vertical"  style="width:30rem;margin-left:2%;margin-right:2%;">
-            <div class="tile">
-                <div class="tile is-parent is-vertical">
-                    <article class="tile is-child notification">
-                        <div class="box" style="height:8rem;">
-                            <article class="media">
-                                <div class="media-content">
-                                    <div class="content">
-                                        <p>
-                                        <strong>{{income.penggunaaktifsebulan}}</strong>
-                                        <br><br>
-                                        <span style="text-transform:uppercase;"> pengguna yang aktif selama 30 hari terakhir</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </article>
-                        </div>
-                    </article>
-                    <article class="tile is-child notification">
-                        <div class="box" style="height:8rem;">
-                            <article class="media">
-                                <div class="media-content">
-                                    <div class="content">
-                                        <p>
-                                        <strong>Rp {{transactionServices.formatPrice(income.perkiraanpenghasilan-income.perkiraanbeban)}} - Rp {{transactionServices.formatPrice(income.perkiraanpenghasilan+income.perkiraanbeban)}}</strong>
-                                        <br><br>
-                                        <span style="text-transform:uppercase;"> perkiraan penghasilan sebulan</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </article>
-                        </div>
-                    </article>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="tile is-ancestor">
-        <div class="tile is-vertical" style="width:30rem;height:20rem;margin-left:2%;margin-right:2%;">
-            <div class="tile">
-                <div class="tile is-parent is-vertical">
-                    <article class="tile is-child notification">
-                        <div class="box" style="height:8rem;">
-                            <article class="media">
-                                <div class="media-content">
-                                    <div class="content">
-                                        <p>
-                                        <strong>{{income.transaksisebulan}}</strong>
-                                        <br><br>
-                                        <span style="text-transform:uppercase;"> transaksi selama 30 hari terakhir</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </article>
-                        </div>
-                    </article>
-                    <article class="tile is-child notification">
-                        <div class="box" style="height:8rem;">
-                            <article class="media">
-                                <div class="media-content">
-                                    <div class="content">
-                                        <p>
-                                        <strong>Rp {{transactionServices.formatPrice((income.perkiraanpenghasilan-income.perkiraanbeban) * 12)}} - Rp {{transactionServices.formatPrice((income.perkiraanpenghasilan+income.perkiraanbeban) * 12)}}</strong>
-                                        <br><br>
-                                        <span style="text-transform:uppercase;"> perkiraan penghasilan setahun</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </article>
-                        </div>
-                    </article>
-                </div>
-            </div>
-        </div>
-    </div>
+    </section>  
 </div>
+
 </template>
 <script>
 
-import TransactionServices from "@/services/TransactionServices"
+    import TransactionServices from "@/services/TransactionServices"
 
-export default {
-  name : "PendapatanBO",
-  data() {
-    return {
-      income: {
-        sebulan: 508000,
-        penggunaaktifsebulan: 307,
-        transaksisebulan: 254,
-        perkiraanbeban: 139000,
-        perkiraanpenghasilan: 567000
-      },
-      transactionServices: new TransactionServices()
+    export default {
+    name : "PendapatanBO",
+    data() {
+        return {
+        income: {
+            sebulan: 508000,
+            penggunaaktifsebulan: 307,
+            transaksisebulan: 254,
+            perkiraanbeban: 139000,
+            perkiraanpenghasilan: 567000
+        },
+        transactionServices: new TransactionServices()
+        };
+    },
     };
-  },
-};
 </script>
 <style scoped>
-.all{
-    display:flex;
-    position: relative;
-}
+    .container {
+        width: 75%;
+        padding-top: 1rem;
+    }
 </style>
